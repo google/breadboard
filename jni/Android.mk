aefe50a91b71eb92db4084f933cb93e2ca101c1c
Jason Sanmiya <jsanmiya@google.com>
2015-09-17 07:45:01 -0700

    Fix Android build break.
    Change-Id: I76ee23e51f389f932da8ce74a9505ff1efab0425
    Tested: sample game builds now for Android

--- jni/Android.mk
+++ jni/Android.mk
@@ -37,7 +37,7 @@ LOCAL_SRC_FILES := \
   $(EVENT_RELATIVE_DIR)/src/event_manager.cpp \
   $(EVENT_RELATIVE_DIR)/src/event_system.cpp \
   $(EVENT_RELATIVE_DIR)/src/graph.cpp \
-  $(EVENT_RELATIVE_DIR)/src/graph_loader.cpp \
+  $(EVENT_RELATIVE_DIR)/src/graph_factory.cpp \
   $(EVENT_RELATIVE_DIR)/src/log.cpp \
   $(EVENT_RELATIVE_DIR)/src/module.cpp \
   $(EVENT_RELATIVE_DIR)/src/node.cpp \
