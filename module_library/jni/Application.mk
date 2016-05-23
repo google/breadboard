d60561ea054757ed4a05cc86cf7032f9c8537fcf
Stewart Miles <smiles@google.com>
2016-05-23 16:26:59 -0700

    Removed reference to deprecated NDK toolchain version.
    Also, APP_MODULES reference to the breadboard_module_library.
    
    Change-Id: Iae4aabfd710825fcac172cfb7008be563d9b6eff

--- module_library/jni/Application.mk
+++ module_library/jni/Application.mk
@@ -14,9 +14,9 @@
 
 APP_STL:=gnustl_static
 APP_ABI := armeabi-v7a x86 armeabi
-NDK_TOOLCHAIN_VERSION := 4.8
+
 APP_CPPFLAGS += -std=c++11 -Wno-literal-suffix
 
-APP_MODULES:=libmodule_library
+APP_MODULES:=libbreadboard_module_library
 
 APP_PLATFORM := android-15
